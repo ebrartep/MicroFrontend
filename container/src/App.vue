6d330dfa4ffd46f05da0f9111cafaf77fa781309
Ebrar TEPECİKLİOĞLU <47458587+ebrartep@users.noreply.github.com>
2022-11-23 03:24:06 +0300

    Add files via upload

---
+++ container/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div>Name: container</div>
+    <Remote1></Remote1>
+    <Remote2></Remote2>
+  </div>
+</template> 
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+const Remote1 = defineAsyncComponent(() => import("remote1/Remote1"));
+const Remote2 = defineAsyncComponent(() => import("remote2/Remote2"));
+
+  export default {
+  name: 'App',
+  components: { 
+    Remote1,
+    Remote2
+   },
+
+}
+</script>
